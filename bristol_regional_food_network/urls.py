dbdaee02ffd3d0fb8c5575f90a632c2a66337a59
goetic-zoe <github@bushycargo.com>
2026-07-05 19:00:10 +0100

    Added home page and updated base template to include a nav bar.
    Signed-off-by: goetic-zoe <github@bushycargo.com>

--- bristol_regional_food_network/urls.py
+++ bristol_regional_food_network/urls.py
@@ -17,6 +17,9 @@ Including another Urlconf
 from django.contrib import admin
 from django.urls import path
 
+from digital_marketplace_platform.views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ]
