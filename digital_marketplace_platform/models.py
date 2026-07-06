990e862fc4bcd93fe6f2b1270f42d5889e36f7d6
goetic-zoe <github@bushycargo.com>
2026-07-06 07:38:11 +0100

    Added product type, allergens, image fields, and updated Order model with delivery details and status choices.
    Signed-off-by: goetic-zoe <github@bushycargo.com>

--- digital_marketplace_platform/models.py
+++ digital_marketplace_platform/models.py
@@ -25,19 +25,45 @@ class User(AbstractUser):
 class Product(models.Model):
     producer = models.ForeignKey(User, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(max_length=500)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     availability_start = models.DateField()
     availability_end = models.DateField()
+
+    TYPES = [
+        ("vegetables", "Vegetables"),
+        ("fruit", "Fruit"),
+        ("dairy_eggs", "Dairy & Eggs"),
+        ("bakery", "Bakery"),
+        ("preserves", "Preserves"),
+        ("seasonal", "Seasonal Specialties"),
+    ]
+    type = models.CharField(max_length=50, choices=TYPES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    allergens = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="product_images/", null=True, blank=True)
 
     def __str__(self):
         return self.name
 
 class Order(models.Model):
     customer = models.ForeignKey('User', related_name='orders', on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product, through='OrderItem')
+    product = models.ManyToManyField(Product, through='OrderItem')
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     order_date = models.DateTimeField(auto_now_add=True)
+    delivery_address = models.TextField(max_length=500)
+    postcode = models.TextField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    STATUS_CHOICES = [
+        ("pending", "Pending"),
+        ("confirmed", "Confirmed"),
+        ("rejected", "Rejected"),
+        ("ready", "Ready"),
+        ("cancelled", "Cancelled"),
+        ("completed", "Completed"),
+    ]
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='pending')
 
     def __str__(self):
         return f'Order {self.id}'
